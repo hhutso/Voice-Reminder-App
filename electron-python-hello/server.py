4f8e35ee788d901712de88ab1f1e903be94d6b7c
gen723 <90296555+geevzo@users.noreply.github.com>
2025-10-24 21:52:02 -0600

    hello wolrddd

---
+++ electron-python-hello/server.py
@@ -0,0 +1,11 @@
+# server.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return jsonify({"message": "Hello from Python!"})
+
+if __name__ == '__main__':
+    app.run(port=5000)
